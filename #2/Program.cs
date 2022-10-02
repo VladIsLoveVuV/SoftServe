ea06e57c4c2af2c6f9617685b31042051bf2e42b
VladIsLoveVuV <114495026+VladIsLoveVuV@users.noreply.github.com>
2022-10-02 16:01:58 +0300

    Add files via upload

---
+++ #2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 6 digit ticket");
+            try
+            {
+                string ticket = Console.ReadLine();
+                if (ticket.Length < 0 && ticket.Length > 6)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                int tickets = Convert.ToInt32(ticket);
+                Console.WriteLine((Convert.ToInt32(Convert.ToString(ticket)[0].ToString()) +
+                                   Convert.ToInt32(Convert.ToString(ticket)[1].ToString()) +
+                                   Convert.ToInt32(Convert.ToString(ticket)[2].ToString())) ==
+                                  (Convert.ToInt32(Convert.ToString(ticket)[3].ToString()) +
+                                   Convert.ToInt32(Convert.ToString(ticket)[4].ToString()) +
+                                   Convert.ToInt32(Convert.ToString(ticket)[5].ToString())) 
+                    ? "Ticket happy"
+                    : "The ticket is not happy");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("End of the program");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
