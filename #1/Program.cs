3ab7620a696b2278659b476c8743e84196c344fc
VladIsLoveVuV <114495026+VladIsLoveVuV@users.noreply.github.com>
2022-10-02 15:39:43 +0300

    Add files via upload

---
+++ #1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace homework1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press the dot to end the program");
+            Console.WriteLine("Enter some symbols: ");
+
+            char input;
+            int spaceCount = 0;
+            do
+            {
+                input = Console.ReadKey().KeyChar;
+                if (input == ' ')
+                    spaceCount++;
+            }
+            while (input != '.');
+
+            Console.WriteLine("Quantity of spaces: " + spaceCount);
+
+        }
+    }
+}
