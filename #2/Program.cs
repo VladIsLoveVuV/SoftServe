d71242846942948ac455546897a3f54ba1086bc8
VladIsLoveVuV <114495026+VladIsLoveVuV@users.noreply.github.com>
2022-10-03 09:09:26 +0300

    Add files via upload

---
+++ #2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 6 digit ticket");
+                string ticket = Console.ReadLine();
+                int tickets = Convert.ToInt32(ticket);
+                Console.WriteLine((Convert.ToInt32(Convert.ToString(ticket)[0].ToString()) +
+                                   Convert.ToInt32(Convert.ToString(ticket)[1].ToString()) +
+                                   Convert.ToInt32(Convert.ToString(ticket)[2].ToString())) ==
+                                  (Convert.ToInt32(Convert.ToString(ticket)[3].ToString()) +
+                                   Convert.ToInt32(Convert.ToString(ticket)[4].ToString()) +
+                                   Convert.ToInt32(Convert.ToString(ticket)[5].ToString()))
+                    ? "Ticket happy"
+                    : "The ticket is not happy");
+        }
+    }
+}
