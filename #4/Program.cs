3e20b7dcb5f0958ddb060291a36d0873a0198631
VladIsLoveVuV <114495026+VladIsLoveVuV@users.noreply.github.com>
2022-10-03 09:40:25 +0300

    Add files via upload

---
+++ #4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is necessary to enter the number N, and N>0");            
+                Console.WriteLine("Enter N");
+                string N = Console.ReadLine();
+                if (Convert.ToInt32(N) < 0)
+                {
+                    Console.WriteLine("Condition N>0 not met");
+                    return;
+                }
+                Console.Clear();
+                Console.WriteLine("Received number");
+                for (int i = N.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(N[i]);
+                }            
+            Console.WriteLine("\nEnd of program");
+        }
+    }
+}
